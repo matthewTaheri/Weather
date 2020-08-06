bbd1a42ea4384070ee484b04563973a63e8fc787
Moses <mahdi2480.t@gmail.com>
2020-08-06 14:19:41 +0430

    ViewModel and Resource added

--- app/src/main/java/com/example/weather/ui/MainActivity.kt
+++ app/src/main/java/com/example/weather/ui/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.weather.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.weather.R
+import com.example.weather.db.WeatherDatabase
+import com.example.weather.repository.WeatherRepository
+import com.example.weather.ui.viewmodels.WeatherViewModel
+import com.example.weather.ui.viewmodels.WeatherViewModelProviderFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: WeatherViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val repository = WeatherRepository(WeatherDatabase(this))
+        val viewModelProviderFactory = WeatherViewModelProviderFactory(repository)
+        viewModel =
+            ViewModelProvider(this, viewModelProviderFactory).get(WeatherViewModel::class.java)
     }
 }
